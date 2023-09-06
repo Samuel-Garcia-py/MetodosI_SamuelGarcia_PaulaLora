bbf19632edb9b2e5bc831f71bdc80e6ce356b6da
Samuel-Garcia-py <samuel.garcia.cast@hotmail.com>
2023-09-06 18:35:02 -0500

    Final_Update

--- Parcial1/Parcial_1.py
+++ Parcial1/Parcial_1.py
@@ -14,6 +14,9 @@ import numpy as np
 def Funcion(x):
     return (math.exp(-x)) - x
 
+def Derivative(f,x,h=1e-10):
+    return (f(x+h)-f(x-h))/(2*h)
+
 # Ahora que ya tenemos la función, hacemos la interpolación cuadrática de Newton.
     
 def definir_x0_x1():
@@ -45,6 +48,8 @@ def interpolacion_cuadratica_newton(x, x0, x1, x2):
     return resultado
 
 
+
+
 # Una vez tenemos la expresión para la diferencia dividida, podemos transformarla a la fórmula 
 # canónica así:
 
@@ -67,25 +72,7 @@ def forma_canonica_newton(x, x0, x1, x2):
         print('Error: División por cero')
         return None, None, None
     
-    def calcular_x3(a, b, c, suma=True):
-        
-        discriminante = b**2 - 4*a*c
-    
-        if discriminante < 0:
-            return None  # No hay raíces reales
-    
-        if suma:
-            x3 = (-2 * c) / (b + math.sqrt(discriminante))
-        else:
-            x3 = (-2 * c) / (b - math.sqrt(discriminante))
-            
-        error = np.abs(Funcion(x3))
-    
-        print (x3)
-        
-
-
-
+# Llamamos a definir_x0_x1 para encontrar x0, x1 y x2
 x0, x1, x2 = definir_x0_x1()
 
 if x0 is not None:
@@ -102,6 +89,62 @@ if x2 is not None:
     print("x2 =", x2)
 else:
     print("No se encontró x2.")
+    
+    def calcular_x3(a, b, c, suma=True):
+        
+        discriminante = b**2 - 4*a*c
+    
+        if discriminante < 0:
+            return None  # No hay raíces reales
+    
+        if suma:
+            x3 = (-2 * c) / (b + math.sqrt(discriminante))
+            return x3
+        else:
+            x3 = (-2 * c) / (b - math.sqrt(discriminante))
+            return x3
+            
+        def GetNewtonMethod(Funcion,Derivada,x3,itmax=100,precision=1e-8):
+    
+        # Este método toma un xn y construye la ecuación de la recta usando la
+        # derivada de f(x) en xn. 
+    
+            error = 1. 
+    
+            it = 0 # Las iteraciones comienzan en 0 y llegan hasta 100 según itmax.
+    
+            while error > precision and it < itmax:
+        
+                try:
+            
+                # Se pretende que el siguiente punto en la iteración 
+                # sea un raíz de f(xn+1) = 0, por tanto:
+                    x0 = x3 - Funcion(x3)/Derivada(Funcion,x3)
+            
+                # Criterio de parada
+                    error = np.abs(Funcion(x3)/Derivada(Funcion,x3))
+            
+                except ZeroDivisionError:
+                    print('Division por cero')
+            
+                x3 = x0
+                it += 1
+        
+        # print('Raiz',xn,it)
+    
+            if it == itmax:
+                return False
+            else:
+                return x3
+            
+            
+            print (x3, error)
+        
+
+
+
+
+    
 
 
 
