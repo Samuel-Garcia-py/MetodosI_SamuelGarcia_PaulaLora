d0dd0cd5e8eba9333e184425eeb925b88ecb0fbd
Samuel-Garcia-py <samuel.garcia.cast@hotmail.com>
2023-11-08 18:48:15 -0500

    UpdateFinal

--- Parcial3/Parcial3.py
+++ Parcial3/Parcial3.py
@@ -5,7 +5,6 @@ Created on Wed Nov  8 17:01:18 2023
 @author: GABI
 """
 
-from __future__ import division, print_function
 import numpy as np
 
 
@@ -56,5 +55,32 @@ def Jacobi(A,b,x0,itmax=5,tolerancia=1e-2):
 A = np.array([[0.2,0.1,1,1,0],[0.1,4,-1,1,-1],[1,-1,60,0,-2], [1,1,0,8,4],[0,-1,-2,4,700]])
 b = np.array([1,2,3,4,5])
 x0 = np.array([1,1,1,1,1])
-print(Jacobi(A, b, x0))
 
+np.linalg.solve(A,b)
+Jacobi(A,b,np.array([0.,0.,0.,0.,0.]))
+
+def GetT(A):
+    
+    M,N = A.shape
+    
+    D = np.zeros((M,N))
+    R = D.copy()
+    T = R.copy()
+    
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            if i == j:
+              #  print(A[i,j])
+               # print(1./A[i,j])
+                D[i,j] = 1./A[i,j]
+            else:
+                R[i,j] = A[i,j]
+    
+ #   print(D)
+ #   print(R)
+    return np.dot(D,R)
+T = GetT(A)
+#print(Jacobi(A, b, x0))
+Values,Vectors = np.linalg.eig(T)
+np.abs(Values)
+print (np.abs(Values))
